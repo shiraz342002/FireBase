7c584e9bc4077464d0157ec5547552eb26d5cf06
Shiraz Mazhar <imshiraz007@gmail.com>
2024-09-20 12:31:06 +0500

    File uploading added

--- FireBase/src/components/Storage.jsx
+++ FireBase/src/components/Storage.jsx
@@ -1,21 +1,24 @@
 import React, { useState } from 'react'
 import { storage } from '../config/FireBaseConfig'
+import { ref, uploadBytes } from 'firebase/storage';
+import { toast } from 'react-toastify';
 const Storage = () => {
     const [fileUpload,setFileUpload]=useState(null)
     console.log(fileUpload);
     
-    const uploadFile=()=>{
-        if(!fileUpload){
-            return
+    const uploadFile=async()=>{
+        if(!fileUpload)return
+        const fileUploadRef=ref(storage,`Testing/${fileUpload.name}`)    
+        try{
+            await uploadBytes(fileUploadRef,fileUploadRef)
+            toast.success("File uploaded Successfully")
+        }catch(err){
+            toast.error("Error Occured "+err)
         }
-        else{
-            
-        }
-
     }
   return (
     <div>
-      <input type="file" onChange={(e)=>setFileUpload(event.target.files[0])} />
+      <input type="file" onChange={(e)=>setFileUpload(e.target.files[0])} />
       <button onClick={uploadFile}>Upload File</button>
     </div>
   )
